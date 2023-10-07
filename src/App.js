845dd0347a803d8612e4daa28d843c8ea107f9ba
Wing Chi So <wingchiso@Wings-MacBook-Pro-3.local>
2023-10-07 20:47:19 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import List from "./components/List";
+import styled from "styled-components";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Container = styled.div`
+  box-shadow: 5px 5px 10px grey;
+  background-color: #EEE3CB;
+  border-radius: 10px;
+  min-height: 150px;
+  margin: 50% 25%;
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 14px 0;
+
+`;
+
+const InnerContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+h3{
+  color: #472D2D;
+margin-top: 5px;
+margin-bottom: 0px;
 }
+`
+
+const Form = styled.form`
+padding: 6% 0 5%;
+
+input{
+  margin-right: 5px;
+}
+
+button{
+  background-color: #C2DBC1;
+  border-color: #F6E3C5;
+  color: #464E2E;
+  font-weight: bold;
+}
+`
+const ClearBtn = styled.button`
+background-color: #BBBBBB;
+height: 35px;
+
+color: #9A8194;
+font-weight: bold;
+border-color: #B7B7B7;
+`
+const getLocalStorage= ()=>{
+  let list = localStorage.getItem("list")
+  if(list) {
+    return list=JSON.parse(localStorage.getItem("list"))
+  } else{
+    return [];
+  }
+}
+
+const App = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [list, setList] = useState(getLocalStorage());
+  const [todoTitle, setTodoTitle] = useState("");
+  const [editID, setEditID] = useState(null);
+
+
+useEffect(()=>{
+  localStorage.setItem("list", JSON.stringify(list))
+}, [list])
+
+
+  const editItem = (id, title) => {
+    setTodoTitle(title);
+    setIsEditing(true);
+    setEditID(id);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!todoTitle) {
+      toast.warning("Please Enter Value", {position: "top-center", autoClose:200});
+    } else if (isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: todoTitle };
+          }
+          return item;
+        })
+      );
+      toast.success("Value Changes", {position: "top-center", autoClose:200});
+      setIsEditing(false);
+      setTodoTitle("");
+    } else {
+      const newItem = { id: new Date().getTime().toString(), title: todoTitle };
+      setList([...list, newItem]);
+      toast.success("Item Added to the List", {position: "top-center", autoClose:200})
+      setTodoTitle("");
+    }
+
+  };
+
+  const removeItem = (id) => {
+    setList(list.filter((item) => item.id !== id));
+    toast.success("Item Removed", {position: "top-center", autoClose:3000})
+    setIsEditing(false);
+  };
+  const clearItems = () => {
+    if (list.length>0){
+    setList([]);
+    toast.success("Empty List", {position: "top-center", autoClose:200})
+    setTodoTitle("")}
+  };
+  
+
+  return (
+    <Container>
+    <InnerContainer>
+      <ToastContainer position="top-center" autoClose={200}/>
+      <h3>Todo list using local storage</h3>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+        ></input>
+        <button type="submit">{isEditing ? "Edit" : "Submit"}</button>
+      </Form>
+      <List items={list} editItem={editItem} removeItem={removeItem} />
+      <ClearBtn onClick={clearItems}>Clear Items</ClearBtn>
+    </InnerContainer>
+    </Container>
+  );
+};
 
 export default App;
---
+++ src/components/List.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import styled from "styled-components";
+
+const ListItemsContainer = styled.div`
+margin-bottom: 6px;`
+
+const ListItemContainer = styled.div`
+background-color: white;
+border-radius: 8px;
+height: 30px;
+display: flex;
+align-items: center;
+justify-content: center;
+margin-bottom: 10px;
+padding: 5px 0;
+
+`
+
+const Ol = styled.ol`
+  display: flex;
+  list-style-type: none;
+  padding: 20px;
+`;
+
+const Li = styled.li`
+  width: 240px;
+  color: #594138;
+`;
+
+const Button = styled.button`
+  border: none;
+  background: none;
+`;
+
+const List = ({ items, editItem, removeItem }) => {
+  return (
+    <ListItemsContainer>
+      {items.map((item, index) => {
+        const { id, title } = item;
+        return (
+          <ListItemContainer key={id}>
+          <Ol >
+            <Li>
+              {index + 1}. {title}
+            </Li>
+            <Button
+              style={{ color: "#557C55" }}
+              onClick={() => editItem(id, title)}
+            >
+              <FaEdit />
+            </Button>
+
+            <Button style={{ color: "#C9485B" }} onClick={() => removeItem(id)}>
+              <FaTrash />
+            </Button>
+          </Ol>
+          </ListItemContainer>
+        );
+      })}
+    </ListItemsContainer>
+  );
+};
+export default List;
